661a44008cdb15435d3d1739782bf26eb8575b1d
Ritta <brittanykelly33@gmail.com>
2022-04-28 00:16:33 -0400

    commit tree 52c2547f1eac02cd99afb888ae9b3779ef817859

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Code your solution here
+const drivers = [
+    {
+        name: 'Bobby',
+        hometown: 'Pittsburgh'
+    },
+    {
+        name: 'Sammy',
+        hometown: 'New York'
+    },
+    {
+        name: 'Sally',
+        hometown: 'Cleveland'
+    },
+    {
+        name: 'Annette',
+        hometown: 'Los Angeles'
+    },
+    {
+        name: 'Bobby',
+        hometown: 'Tampa Bay'
+    }
+];
+
+function findMatching(array, string) {
+    return array.filter(function (element) {
+        return element.toLowerCase().indexOf(string.toLowerCase()) !== -1
+    })
+}
+
+function fuzzyMatch(array, string) {
+    return array.filter(function (element) {
+        return element.startsWith(string);
+    })
+}
+
+function matchName(drivers, name) {
+    return drivers.filter(function (driver) {
+        return driver.name === name
+    })
+}
